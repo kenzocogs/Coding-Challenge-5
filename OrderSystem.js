efb96a6d682d26b1b883768460fee1701cd235ed
Kenzo Cogswell <kcogswell@usf.edu>
2024-09-18 21:07:48 -0400

    Initialize inventory with product objects

---
+++ OrderSystem.js
@@ -0,0 +1,28 @@
+// Task 1: Create an Inventory of Product Objects
+
+let inventory = [
+{ 
+    name: 'CaramelLatte',
+    price: 8,
+    quantity: 12, 
+},
+
+{ 
+    name: 'ColdBrew',
+    price: 10,
+    quantity: 18, 
+},
+
+{ 
+    name: 'Macchiato',
+    price: 5,
+    quantity: 7, 
+},
+
+{ 
+    name: 'Americano',
+    price: 9,
+    quantity: 13, 
+},
+
+];
